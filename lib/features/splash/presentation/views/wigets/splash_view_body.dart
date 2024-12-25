f17f3c480beb84922bb0874a2dcfbbd61e518092
Win -11 <sari.ahmed25@gmail.com>
2024-12-25 19:42:46 +0200

    adding constats and assets for splash screen

--- lib/features/splash/presentation/views/wigets/splash_view_body.dart
+++ lib/features/splash/presentation/views/wigets/splash_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
 class SplashViewBody extends StatelessWidget {
@@ -5,6 +6,15 @@ class SplashViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(AssetsData.logo),
+      const SizedBox(height: 4)
+      ,
+      Text('Read Free Books', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),textAlign: TextAlign.center,)
+      ],
+    );
   }
 }
