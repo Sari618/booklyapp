f3a4f29350acfac00865ea0c20cefee3d42654f7
Win -11 <sari.ahmed25@gmail.com>
2024-12-25 19:48:42 +0200

    ay haga

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class BooklyApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor:  kPrimaryColor),
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor:  Colors.white),
       home: const SplashView(),
     );
   }
